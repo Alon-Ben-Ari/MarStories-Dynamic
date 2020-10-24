bc7c66d54b07f7cf230ef14a54b9d23b5d90f566
Alon Ben Ari <alon.b.a@outlook.com>
2020-10-24 18:30:52 +0300

    Add favicon

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
 
 
-@app.route('/<rover_name>/<sol>')
-def post_site(rover_name, sol):
-    resp = requests.get(
-        f'https://mars-photos.herokuapp.com/api/v1/rovers/{rover_name}/photos?sol={sol}')
+@app.route('/')
+def home_page():
+    rover_name = request.args.get('rover')
+    sol = request.args.get('sol')
+    date = request.args.get('date')
+    if not rover_name:
+        return render_template('index.html', args_given=False)
+    if sol:
+        resp = requests.get(
+            f'https://mars-photos.herokuapp.com/api/v1/rovers/{rover_name}/photos?sol={sol}')
+    elif date:
+        resp = requests.get(
+            f'https://mars-photos.herokuapp.com/api/v1/rovers/{rover_name}/photos?earth_date={date}')
     resp_json = resp.json()
     images_data = resp_json['photos']
     return render_template(
         'index.html',
-        images_data=images_data
+        images_data=images_data,
+        args_given = True
     )
